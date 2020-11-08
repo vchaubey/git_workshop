2271e7958b8ca2984f06d02ae72286111103155d
Brian Burnstein <bhburns@umich.edu>
2020-11-08 13:39:14 -0500

    add files

---
+++ exercise_1.py
@@ -0,0 +1,16 @@
+MEMBER_1 = "Terry"\
+MEMBER_2 = "Allen"
+MEMBER_3 = "Ramon"
+
+MEMBER_1_HOME = "Richmond, VA"
+MEMBER_2_HOME = "Haiphong, Viet Nam"
+MEMBER_3_HOME = "Monterrey, Mexico"
+
+MEMBERS = {
+    MEMBER_1:MEMBER_1_HOME,
+    MEMBER_2:MEMBER_2_HOME,
+    MEMBER_3:MEMBER_3_HOME,
+}
+
+for k,v in MEMBERS.items():
+    print(f"{k} is from {v}")
